e31ae64c6d9dc1a7a009badfd68d9736b7514753
atila <atila.vini@hotmail.com>
2024-08-21 11:58:35 -0300

    build: new fresh from laravel/sail, fix miss conection to db

--- mxmz-contact/routes/web.php
+++ mxmz-contact/routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ContactFormController;
 
 Route::get('/', function () {
-    return view('form');
+    return view('welcome');
 });
-
-Route::get('/contact', [ContactFormController::class, 'showForm'])->name('contact.form');
-Route::post('/contact', [ContactFormController::class, 'submitForm']);
