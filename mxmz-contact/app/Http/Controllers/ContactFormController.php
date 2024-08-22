ef394f66a65e991d270ffdbc4a021a90e88e4c06
atila <atila.vini@hotmail.com>
2024-08-22 13:50:17 -0300

    feat: implement validation of data form

--- mxmz-contact/app/Http/Controllers/ContactFormController.php
+++ mxmz-contact/app/Http/Controllers/ContactFormController.php
@@ -14,13 +14,21 @@ class ContactFormController extends Controller
 
     public function submitForm(Request $request)
     {
-        $request->validate([
+        $validatedData = $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|email|max:255',
-            'phone' => 'required|string|max:15',
-            'summary' => 'required|string|max:500',
-            'description' => 'required|string',
-            'file' => 'nullable|file|mimes:pdf,doc,docx|max:2048',
+            'phone' => ['required', 'regex:/^\d{10,15}$/'],
+            'summary' => ['required', 'string', 'max:255', 'regex:/^[a-zA-Z0-9\s]*$/'],
+            'description' => ['required', 'string', 'max:1000', 'regex:/^[a-zA-Z0-9\s]*$/'],
+            'file' => 'required|file|mimes:pdf|max:9216',
+        ], [
+            'name.required' => 'Por favor, insira seu nome.',
+            'email.required' => 'O campo de email é obrigatório.',
+            'phone.required' => 'O número de telefone é obrigatório.',
+            'summary.required' => 'Por favor, forneça um resumo profissional.',
+            'description.required' => 'A descrição é necessária.',
+            'file.required' => 'Um arquivo deve ser anexado.',
+            'file.mimes' => 'O arquivo deve ser um PDF.',
         ]);
 
     $filePath = null;
@@ -30,15 +38,15 @@ class ContactFormController extends Controller
     }
 
         ContactForm::create([
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
-            'phone' => $request->input('phone'),
-            'summary' => $request->input('summary'),
-            'description' => $request->input('description'),
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'phone' => $validatedData['phone'],
+            'summary' => $validatedData['summary'],
+            'description' => $validatedData['description'],
             'file' => $filePath,
         ]);
 
-        return back()->with('success', 'Form submitted successfully!');
+        return back()->with('success', 'Informações enviadas com sucesso!');
     }
 }
 
